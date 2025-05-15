d774b49927190aeb185442408fc594e13f70bcf1
Felipe Silva <felipe.silva@webtech87.pt>
2025-05-15 15:41:30 +0100

    [UPDATE] Add email reception with form data

--- backend/app.py
+++ backend/app.py
@@ -4,11 +4,19 @@ import os
 from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect
 from dotenv import load_dotenv
+from flask_mail import Message, Mail
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('GMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('GMAIL_PASS')
+
+mail = Mail(app)
 
 csrf = CSRFProtect(app)
 
@@ -20,8 +28,24 @@ CORS(app, origins=[
 @app.route('/api/contact', methods=['POST'])
 def contact():
     form = ContactForm(data=request.json, meta={'csrf': False})
-
+    
     if form.validate():
+        msg = Message (
+            subject='New Contact Form Submission',
+            sender=app.config['MAIL_USERNAME'],
+            recipients=[app.config['MAIL_USERNAME']],
+            html= f'''
+            <h1>New Form Received</h1>
+            <br>
+            <p><strong>Name:</strong> {form.name.data}</p>
+            <p><strong>Email:</strong> {form.email.data}</p>
+            <p><strong>Phone:</strong> {form.phone.data}</p>
+            <p><strong>Message:</strong><br>{form.message.data}</p>
+            '''
+        )
+        
+        mail.send(msg)
+
         return jsonify({'sucess': True, 'message': 'Form sent successfully'}), 200
     else:
         print("----------------------------", form.errors)
